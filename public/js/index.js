8cf165e89c541015a195d307efd72ddaafdd1806
Jetender <g.jetender@gmail>
2019-02-05 00:44:31 -0500

    needs some work

--- public/js/index.js
+++ public/js/index.js
@@ -1,47 +1,102 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
+
 var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+var submitSearch = $("#submitSearch");
+var newType = $("#typeV");
+var newMake = $("#typeMake");
+var newModel = $("#typeModel");
+var newPrice = $("#typePrice");
+var newDescrpt = $("#typeDes");
+var newYear = $("#typeYear")
+var newImage = $("");
+var modSea = $("#btnmodel");
+var makeSea = $("#btnmake");
+var yearSea = $("#btnyear")
+var priceSea = $("#btnprice");
+
+
+//objectVehicle
+
+var popularVehicle = ["Ford", "Toyoto", "Honda", "Nissan", "Chevrolet", "Hyundai", "Ram", "Volksvagen", "GMC", "KIA", "Jeep", "Subaru", "Mazada", "Mercedes_Benz", "BMW", "Dodge"];
+
+
+function callmake() {
+  $.ajax({
+    url: "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/honda?format=json",
+    success: function (result) {
+      //manually set up a append option likes orginally do for each statment is needed
+      console.log(result)
+
+
+    }
+  })
+}
+
+callmake();
+
+
+$("button").click(function () {
+  $.ajax({
+    url: "demo_test.txt",
+    success: function (result) {
+      $("#div1").html(result);
+    }
+  });
+});
+///////
+
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveOne: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "api/examples",
+      url: "api/vehicle",
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getAll: function () {
     return $.ajax({
-      url: "api/examples",
+      url: "api/vehicle",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  getMake: function () {
     return $.ajax({
-      url: "api/examples/" + id,
+      url: "api/vehicle/" + make,
+      type: "GET"
+    });
+  },
+  getModel: function () {
+    return $.ajax({
+      url: "api/vehicle/" + make + model,
+      type: "GET"
+    });
+  },
+
+  deleteOne: function (id) {
+    return $.ajax({
+      url: "api/vehicle/" + id,
       type: "DELETE"
     });
   }
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshData = function () {
+  API.getAll().then(function (data) {
+    var $newitem = data.map(function (example) {
       var $a = $("<a>")
-        .text(example.text)
+        .text(data)
         .attr("href", "/example/" + example.id);
 
       var $li = $("<li>")
         .attr({
           class: "list-group-item",
-          "data-id": example.id
+          "data-id": data
         })
         .append($a);
 
@@ -55,45 +110,66 @@ var refreshExamples = function() {
     });
 
     $exampleList.empty();
-    $exampleList.append($examples);
+    $exampleList.append($newitem);
   });
 };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+  console.log(newModel);
+  var newVehicle = {
+    vehicle: newType.val().trim(),
+    make: newMake.val().trim(),
+    model: newModel.val().trim(),
+    price: newPrice.val(),
+    description: newDescrpt.val().trim(),
+    year: newYear.val()
   };
+  console.log(newVehicle)
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
+  if (!(newVehicle.vehicle && newVehicle.make && newVehicle.model)) {
+    alert("Enter information");
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveOne(newVehicle).then(function () {
     refreshExamples();
   });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
+  newType.val("");
+  newMake.val("");
+  newModel.val("");
+  newPrice.val("");
+  newDescrpt.val("");
+  newYear.val("");
+  newDescrpt.val("");
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
+  API.deleteOne(idToDelete).then(function () {
+    refreshData();
   });
 };
+//ajax call to api with models, make , year
 
+var searchDatabase = function () {
+  var selectedText = $(this).find("option:selected").text();
+
+  console.log(makeSea.text(selectedText))
+  console.log(modSea.text(selectedText))
+  console.log(yearSea.text(selectedText))
+
+
+}
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+submitSearch.on("click", searchDatabase);
+//$exampleList.on("click", ".delete", handleDeleteBtnClick);
+//get all content from database
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,23 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.vehicle.findAll({}).then(function (vehicleInfo) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        vehicle: vehicleInfo,
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.vehicle.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,7 +25,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
