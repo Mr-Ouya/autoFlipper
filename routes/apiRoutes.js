08bfe2d6f12f1e0cf9cac248c86c8593aa72b667
Jetender <g.jetender@gmail>
2019-02-11 19:52:38 -0500

    Efsfesfs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -29,21 +29,58 @@ module.exports = function (app) {
   app.get("/api/vehicle/:make/:model/:yearmin/:yearmax/:pricemin/:pricemax", function (req, res) {
     console.log(req.params + "hello")
     //console.log(req.body);
+    let whereClause = {
 
+    }
+
+    //Check if Make has been selected 
+    if (req.params.make === null) {} else {
+
+      whereClause.model = req.params.make;
+
+    }
+    if (req.params.make.length == 0) {
+      res.status(404).json({
+        error: 'Make has not been selected'
+      })
+    }
+
+    if (req.params.model) {
+      whereClause.model = req.params.model;
+    }
+
+    if (req.params.yearmin) {
+      whereClause.model = req.params.make;
+    }
+
+
+
+    if (req.params.model) {
+      whereClause.model = req.params.model;
+    }
+    // whereClause.year = {
+    //   [Op.between]: [req.params.yearmin, req.params.yearmax]
+    // }
+
+
+    if (whereClause.length == 0) {
+      res.status(404).json(data);
+      console.log(data);
+    }
 
     db.vehicle.findAll({
+      where: whereClause
+      // where: {
+      //   model: req.params.model,
+      //   make: req.params.make,
+      //   year: {
+      //     [Op.between]: [req.params.yearmin, req.params.yearmax]
+      //   },
+      //   price: {
+      //     [Op.between]: [req.params.pricemin, req.params.pricemax]
+      //   }
 
-      where: {
-        model: req.params.model,
-        make: req.params.make,
-        year: {
-          [Op.between]: [req.params.yearmin, req.params.yearmax]
-        },
-        price: {
-          [Op.between]: [req.params.pricemin, req.params.pricemax]
-        }
-
-      }
+      // }
 
     }).then(function (data) {
       res.json(data);
